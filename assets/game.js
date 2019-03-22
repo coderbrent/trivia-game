ad0e2fdaf9317a3ccc7e45c0f88f1f1efe5595d6
Brent Abruzese <brent.abruzese@gmail.com>
2019-03-22 15:35:18 -0400

    lots of changes

--- assets/game.js
+++ assets/game.js
@@ -1,83 +1,111 @@
-var userCorrect = 0;
-var userIncorrect = 0;
+var userScore = 0;
+var questionList;
+var qTimer = 15;
+var userChoice;
+var questionTimer;
+var userCorrectAnswer;
+var currQues;
+var randNum;
 
-var myQuestions = {
-   questionOne: {
+var myQuestions = [
+    {
         qtext: "What was the name of the Vampire in Nosferatu?",
-        answers: ["Count Orlok", "Norman Bekkler", "Phil Donohue", "Stanley Steemer", "My asshole brother from Los Angeles"]
+        answers: ["Count Orlok", "Norman Bekkler", "Phil Donohue", "Stanley Steemer", "My asshole brother from Los Angeles"],
+        rightAnswer: "Count Orlok"
     },
-    questionTwo: {
+    {
         qtext: "What movie is Boris Karloff most well known for?",
-        answers: "Frankenstein",
-        incorrectAnswers: ["Bend it like Boris", "The Green Pile", "Ferris Buellers Day At Work", "Dracula"]
+        answers: ["Bend it like Boris", "The Green Pile", "Ferris Buellers Day At Work", "Dracula", "Frankenstein"],
+        rightAnswer: "Frankenstein"
     },
-    questionThree: {
+    {
         qtext: "In the movie, the Changeling, what is the name of the boy spirit haunting John Russell's gloomy abode?",
-        correctAnswer: "Joseph",
-        incorrectAnswers: ["Lars", "Tim", "Brett", "Pete Klaven"]
+        answers: ["Lars", "Joseph", "Flea", "Pete Klaven", "Will Smith"],
+        rightAnswer: "Joseph"
+    },
+    {
+        qtext: "What Italian city was the movie Don't Look Now primarily filmed in?",
+        answers: ["Abruzzi", "Rome", "Milan", "Venice", "Naples"],
+        rightAnswer: "Venice"
     }
-}
+    
+]
 
-$(window).on("load", function() {
-    $(".question-panel").toggleClass("invisible", true);
-})
 
-$(".start-quiz").on("click", function() {
-    $(".question-panel").toggleClass("invisible", false);
-    questionOne();
-});
+$(".start-quiz").on("click", playGame); //button the opening game panel that starts the game
 
-function questionOne() {
-    $("#qTimer").text(qTimer);
-    $("#question-body").text(myQuestions.questionOne.qtext);
-    $(".answerBtn").text(myQuestions.questionOne.answers[0]);
-    $(".answerBtn").text(myQuestions.questionOne.answers[1]);
-    $(".answerBtn").text(myQuestions.questionOne.answers[2]);
-    $(".answerBtn").text(myQuestions.questionOne.answers[3]);
-    $(".answerBtn").text(myQuestions.questionOne.answers[4]);
+$(document).on("load", $("#question-panel").addClass("d-none")); //scoped to the document - hides the question panel on page load
 
-    var qTimer = 10;
-
-var daTimer = setInterval(function() {
-    --qTimer;
-    $("#qTimer").html(qTimer);
-    if(qTimer === 0) {
-        clearInterval(daTimer);
-        qTimer = 0;
-        userIncorrect += 1;
-        $(".alert").toggleClass("hidden", false);
+$(document).on("click", ".answerBtn", function() {
+    userChoice = $(this).attr("data-name");
+    currQues = myQuestions[randNum].answers;
+    if(userChoice === myQuestions[randNum].rightAnswer) {
+    clearInterval(questionTimer);
+    correctAnswer(); //we run the correctAnswer function if the player clicks the correct button.
     } else {
-        return;
+        clearInterval(questionTimer);
+        currQues.pop;
     }
-}, 1000)
+ })
 
-$(".answerBtn").on("click", function() {
-    var correctOmundo = $(".answerBtn").hasClass("correct");
-    if(correctOmundo) {
-        clearInterval(daTimer);
-        qTimer = 0;
-        userCorrect += 1;
-        console.log("that is correct");
-    } else {
-        clearInterval(daTimer);
-        qTimer = 0;
-        userIncorrect +=1;
-        console.log("that is incorrect");
-    }
-})
-
-  
-}
-
-function questionTwo() {
-    $("#question-body").text(myQuestions.questionTwo.qtext);
-    $(".answerBtn1").text(myQuestions.questionTwo.correctAnswer);
-    $(".answerBtn2").text(myQuestions.questionTwo.incorrectAnswers[0]);
-    $(".answerBtn correct").text(myQuestions.questionTwo.incorrectAnswers[1]);
-    $(".answerBtn3").text(myQuestions.questionTwo.incorrectAnswers[2]);
+function playGame() { //displays the game panel and question panel - generates the first question and starts the click (baked into generateQuestion).
+    $(".game-panel").addClass("d-none");
+    $("#question-panel").addClass("d-inline");
+    generateQuestion();
 }
 
 
+function wrongAnswer() {
+    clearInterval(questionTimer);
+    userIncorrect--;
+  }
+
+
+function correctAnswer() {  //when the user gets an answer right clear the panel - load the next question... the previous question should not show up again.
+  $("#questList").empty();
+  $("#answerPanel").empty();
+  myQuestions[randNum].answers.pop; //clears both question and answer choice divs
+  generateQuestion();
+  qTimer = 15;
+  }
+
+function generateQuestion() {
+  questionPop = $("#questList");
+  b = 0
+  randNum = Math.floor(Math.random([b]) * 3 + 1)    //is this line scoped right?
+  questionPop.append(myQuestions[randNum].qtext);
+
+  userCorrectAnswer = myQuestions[0].rightAnswer;
+    var userSelection = $(".answerBtn").on("click", function () {
+        document.getElementById("answerBtn").value;
+        console.log(userSelection);
+    })
+
+    genAnswerButtons();
+
+    questionTimer = setInterval(function(){
+      qTimer--;
+        $("#qTimer").html(qTimer);
+          if(qTimer <= 0) {
+            clearInterval(questionTimer);
+            console.log("times up!");
+            $("#correct-answer").append(myQuestions[randNum].rightAnswer);
+            $(".alert").show();
+        }
+    }, 1000);
+   
+}
+    
+function genAnswerButtons() {
+    $("#answerPanel").empty();
+    for(i = 0; i < myQuestions[randNum].answers.length; i++) {
+        var newButt = $("<button>");
+        newButt.addClass("answerBtn m-1");
+        newButt.attr("data-name", myQuestions[randNum].answers[i]);
+        newButt.text(myQuestions[randNum].answers[i]);
+        newButt.appendTo("#answerPanel");
+    }
+  }
 
 
 
